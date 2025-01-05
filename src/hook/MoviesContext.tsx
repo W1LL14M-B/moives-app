7e4809ec1a65f94fd3eeaec49e7fd0465fb6053f
William <wibepu12@gmail.com>
2025-01-04 22:52:08 -0500

    implementando detalles en vista

--- src/hook/MoviesContext.tsx
+++ src/hook/MoviesContext.tsx
@@ -1,23 +1,20 @@
-
-import axios from "axios"
+import axios from "axios";
 import { createContext, ReactNode, useContext, useState } from "react";
 
-
-const API_URL = 'https://api.themoviedb.org/3'
-const API_KEY = '961641fcc7f5229c2503cc1c88ef251e'
-const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+const API_URL = "https://api.themoviedb.org/3";
+const API_KEY = "961641fcc7f5229c2503cc1c88ef251e";
+const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 
 interface Genre {
-  id:number;
+  id: number;
   name: string;
 }
 
-
-interface actor {
+interface Actor {
   id: number;
   name: string;
   profile_path: string;
-} 
+}
 
 interface Movie {
   id: number;
@@ -29,97 +26,91 @@ interface Movie {
   vote_average: number;
 }
 
-
 interface MoviesContextProps {
-  movies: Movie [];
-  fetchMovies: ( query: string, page: number) => Promise<void>;
-  getImageUrl:(path: string) => string;
- //getMovieDetails : ( id: number) => Promise<Movie | null >; 
+  movies: Movie[];
+  fetchMovies: (query: string, page: number) => Promise<void>;
+  getImageUrl: (path: string) => string;
   totalPages: number;
-
-
-
+  getMovieDetails: (id: number) => Promise<Movie | null>;
 }
 
-const MoviesContext = createContext<MoviesContextProps | undefined> (undefined);
+const MoviesContext = createContext<MoviesContextProps | undefined>(undefined);
 
+export const MoviesProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [movieDetails, setMovieDetails] = useState<Movie | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
-export const MoviesProvider: React.FC <{children: ReactNode}> = ({children}) => {
-const [ movies, setMovies] = useState<Movie []>([]);
+  const fetchMovies = async (query: string = "", page: number = 1) => {
+    try {
+      setMovies([]);
+      const endpoint = query
+        ? `${API_URL}/search/movie`
+        : `${API_URL}/movie/popular`;
 
-const [totalPages, setTotalPages] = useState<number>(0);
+      const response = await axios.get(endpoint, {
+        params: {
+          api_key: API_KEY,
+          query,
+          page,
+        },
+      });
 
- 
+      const { results, total_pages } = response.data;
+      setMovies(results);
 
+      setTotalPages(total_pages);
+    } catch (error) {
+      console.error("Error fectching movies:", error);
+    }
+  };
 
+  const getImageUrl = (path: string) => {
+    return `${IMAGE_PATH}${path}`;
+  };
 
+  const getMovieDetails = async (id: number) => {
+    try {
+      const response = axios.get(`${API_URL}/movie/${id}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
 
-const fetchMovies = async ( query: string = "", page: number = 1) => {
+      const movie = (await response).data;
+      const castResponse = await axios.get(`${API_URL}/movie/${id}/credits`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
 
-  try{
-  setMovies([])
-const endpoint = query 
-? `${API_URL}/search/movie`  
-: `${API_URL}/movie/popular`;
+      const actors= castResponse.data.cast;
+      movie.actors = actors;
 
-const response = await axios.get (endpoint, {
-  params: {
-    api_key: API_KEY,
-    query,
-    page,
-  },
-    });
+      setMovieDetails(movie);
+      return movie;
+    } catch (error) {
+      console.error("Error detalles:", error);
+      return null;
+    }
+  };
 
-    const { results, total_pages } = response.data
-    setMovies(results)
+  return (
+    <MoviesContext.Provider
+      value={{ movies, fetchMovies, getImageUrl, totalPages, getMovieDetails }}
+    >
+      {children}
+    </MoviesContext.Provider>
+  );
+};
 
-    setTotalPages(total_pages)
-
- 
-
-  } catch ( error )  {
-    console.error ('Error fectching movies:', error)
-  } 
-}
- 
-const getImageUrl = (path: string) => { 
-  return `${IMAGE_PATH}${path}`
-}
-/* 
-const getMovieDetails = async (id: number) : Promise<MovieDetalle | null > =>  {
-try {
-  const response = axios.get(`${API_URL}/movie/${id}`,{
-
-params: {
-api_key: API_KEY
-
-
-}
-})
- */
-
-
-return (
-  <MoviesContext.Provider value ={{movies, fetchMovies, getImageUrl, totalPages}}>
-    {children}
-
-  </MoviesContext.Provider>
-
- 
- )
-
-}
-
-export const useMovies = () : MoviesContextProps => {
+export const useMovies = (): MoviesContextProps => {
   const context = useContext(MoviesContext);
-if (!context) {
-  throw new Error ('useMovie debe usarse dentro de MovieProvider')
-}
+  if (!context) {
+    throw new Error("useMovie debe usarse dentro de MovieProvider");
+  }
 
-return context;
-
-}
-
-
-
-
+  return context;
+};
