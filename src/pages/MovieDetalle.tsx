199363fdff68de35918ed0984623b585be75f582
William <wibepu12@gmail.com>
2025-01-06 23:19:35 -0500

    implementacion favoritos

--- src/pages/MovieDetalle.tsx
+++ src/pages/MovieDetalle.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
+
 const API_URL = "https://api.themoviedb.org/3";
 const API_KEY = "961641fcc7f5229c2503cc1c88ef251e";
 const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
 
 interface Genre {
   id: number;
@@ -16,6 +18,13 @@ interface Cast {
   name: string;
   profile_path: string | null;
 }
+
+interface Trailer {
+  key: string;
+  name: string;
+}
+
+
 
 interface Movie {
   title: string;
@@ -30,6 +39,7 @@ const MovieDetalle = () => {
   const { id } = useParams<{ id: string }>();
   const [movie, setMovie] = useState<Movie | null>(null);
   const [cast, setCast] = useState<Cast[]>([]);
+  const [trailerkey, setTrailerKey] = useState<Trailer | null>(null);
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -41,9 +51,22 @@ const MovieDetalle = () => {
         const castReponse = await axios.get(`${API_URL}/movie/${id}/credits`, {
           params: { api_key: API_KEY },
         });
+        console.log("card:", castReponse);
+
+        const trailerResponse = await axios.get(
+          `${API_URL}/movie/${id}/videos`,
+          {
+            params: { api_key: API_KEY },
+          }
+        );
+        console.log("trailer", trailerResponse);
 
         setMovie(movieResponse.data);
         setCast(castReponse.data.cast.slice(0, 5));
+        if (trailerResponse.data.results.length > 0) {
+          setTrailerKey(trailerResponse.data.results[0]);
+        }
+     
       } catch (error) {
         console.error("Error fetching movie details osr cast:", error);
       }
@@ -80,36 +103,45 @@ const MovieDetalle = () => {
                 {movie.genres.map((genre) => genre.name).join(",")}
               </p>
             </div>
-            <div className="col-md-6">
-            <h2>actores principales</h2>
-            <div className="row">
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h2>Actores principales</h2>
+          <div className="row">
             <ul>
               {cast.map((actor) => (
                 <li key={actor.id} className="col-6 col-md-4 mb-4">
                   <div className="card">
-                  <p>{actor.name}</p>
-                  {actor.profile_path && (
-                    <img
-                      src={`${IMAGE_PATH}${actor.profile_path}`}
-                      alt={actor.name}
-                      className="card-img-top"
-                    />
-                  )}
-
+                    <p>{actor.name}</p>
+                    {actor.profile_path && (
+                      <img
+                        src={`${IMAGE_PATH}${actor.profile_path}`}
+                        alt={actor.name}
+                        className="card-img-top"
+                      />
+                    )}
                   </div>
-                 
                 </li>
               ))}
             </ul>
-
-            </div>
-          
-
-            </div>
-
-          
           </div>
         </div>
+
+{trailerkey && (
+              <div className="card-body">
+                <h3>Trailer</h3>
+                <div className="embed-responsive embed-responsive-16by9">
+                  <iframe
+                    className="embed-responsive-item"
+                    src={`https://www.youtube.com/embed/${trailerkey}?autoplay=1`}
+                    title={trailerkey.name}
+                    allowFullScreen
+                 
+                  ></iframe>
+                </div>
+              </div>
+            )}
+ 
       </div>
     </div>
   );
--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,24 +1,71 @@
 import { useEffect, useState } from "react";
-import { useMovies } from "../hook/MoviesContext";
+import { MoviesProvider, useMovies } from "../hook/MoviesContext";
 import { Link } from "react-router-dom";
 
 const Movies: React.FC = () => {
   const { movies, fetchMovies, getImageUrl } = useMovies();
   const [query, setquery] = useState<string>("");
   const [page, setPage] = useState<number>(1);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [showFavorites,setShowFavorites] = useState<boolean>(false)
 
   useEffect(() => {
     fetchMovies(query, page);
   }, [query, page]);
 
+  useEffect(() => {
+    const storeFavorites = localStorage.getItem("favorites");
+    if (storeFavorites) {
+      setFavorites(JSON.parse(storeFavorites));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (favorites.length > 0) {
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    }
+  }, [favorites]);
+
   const handleNextPage = () => setPage((prevPage) => prevPage + 1);
   const handlePreviosPage = () =>
     setPage((prevPage) => Math.max(prevPage - 1, 1));
 
+  const toggleFavorite = (MovieId: number) => {
+    setFavorites((prevFavorites) => {
+      if (prevFavorites.includes(MovieId)) {
+        return prevFavorites.filter((id) => id !== MovieId);
+      } else {
+        return [...prevFavorites, MovieId];
+      }
+    });
+  };
+
+
+  const toggleShowFavorites = () => {
+setShowFavorites((prevShowFavorites) => !prevShowFavorites)
+  }
+
+  const moviesToShow = showFavorites
+  ? movies.filter((movie) => favorites.includes(movie.id))
+  : movies;
+
   return (
     <div className="container mt-4">
+      <div className="d-flex justify-content-between">
+        <button
+        onClick={toggleShowFavorites}
+        className="btn btn-link"
+
+        >
+{showFavorites ? "Ver todas las peliculas" : "ver solo favorites"}
+
+        </button>
+
+
+
+      </div>
       <div className="row">
-        {movies.map((movie) => (
+        {moviesToShow.map((movie) => (
           <div key={movie.id} className="col-md-3 mb-4">
             <div className="card h-100">
               <img
@@ -30,7 +77,20 @@ const Movies: React.FC = () => {
               <div className="card-body">
                 <h5 className="card-title">{movie.title}</h5>
                 <p className="card-text-truncante">{movie.overview}</p>
-                
+
+                <button
+                  onClick={() => toggleFavorite(movie.id)}
+                  className={`btn ${
+                    favorites.includes(movie.id)
+                      ? "btn-warning"
+                      : "btn-outline-warning"
+                  }`}
+                >
+                  {favorites.includes(movie.id)
+                    ? "Quitar de favoritos"
+                    : "Añadir a favoritos"}
+                </button>
+
                 <Link to={`/movies/${movie.id}}`} className="btn btn-link">
                   Mas...
                 </Link>
